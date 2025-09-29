4c8d580b6c5823ae9e0fca0b0fb89e83cb132f6c
luehaoshen <luehaoshen@gmail.com>
2025-09-29 16:34:57 +0800

    Update Database.h

--- Database.h
+++ Database.h
@@ -1,11 +1,12 @@
-#ifndef DATABASR_H
+#ifndef DATABASE_H
 
-#define DATABASR_H
+#define DATABASE_H
 
 int database_create(const char *db_path, const char *table_name, const char *schema);
 
 int database_insert(const char *db_path, const char *table_name, const char *columns, const char *values);
 
-int database_select(const char *db_path, const char *table_name, const char *columns, const char *condition);
+int database_select(const char *db_path, const char *table_name, const char *columns, const char *condition,
+                    char **result, size_t max_len);
 
 #endif
